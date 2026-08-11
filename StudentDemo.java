897977e1008c98fbfac43f0d6dcce634ab01ce8c
agilan35 <gakilan9@gmail.com>
2026-08-11 14:52:50 +0530

    Add files via upload

--- StudentDemo.java
+++ StudentDemo.java
@@ -1,34 +1,44 @@
-public class StudentDemo { 
+public class StudentDemo{
+	static class Student{
+	int rollNo;
+	String name; 
 
-    static class Student { 
-        int rollNo; 
-        String name;  
-        
-        Student(int r, String n) { 
-            rollNo = r; 
-            name = n; 
-            System.out.println("Constructor called for Roll No: " + rollNo); 
-        } 
-
-        void display() { 
-            System.out.println("Roll No: " + rollNo + "\tName: " + name); 
-        } 
-    } 
-
-    public static void main(String[] args) throws InterruptedException { 
-        System.out.println("----- Creating array of objects -----"); 
-        Student[] s = new Student[3]; 
-        s[0] = new Student(101, "Arun"); 
-        s[1] = new Student(102, "Bala"); 
-        s[2] = new Student(103, "Chitra"); 
-
-        System.out.println("\n----- Displaying student details -----"); 
-        for (int i = 0; i < s.length; i++) 
-            s[i].display(); 
-
-        System.out.println("\n----- Dereferencing objects ----"); 
-        for (int i = 0; i < s.length; i++) 
-            s[i] = null; 
-        System.out.println("Objects are now eligible for garbage collection."); 
-    } 
+	Student(int r, String n) {
+		rollNo =r;
+		name =n;
+		System.out.println("Constructor called for Rollno:" + rollNo);
+		}
+	void display() {
+		System.out.println("Roll No:" + rollNo + "\t Name:" + name);
+		}
+	protected void Finalize() {
+		System.out.println("Destructor(finalize) called for Roll No:" + rollNo);
+	}
 }
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("\n=====Creating Array of Object=====");
+	Student[]s = new Student [3];
+	s[0] = new Student (101, "Light Yagami");
+	s[1] = new Student (102, "Son Goku");
+	s[2] = new Student (103, "Toji Fushiguro");
+
+	System.out.println("\n=====Displaying Student Details=====");
+	for(int i = 0; i < s.length ; i++)
+		s[i].display();
+
+	System.out.println("\n=====Deferencing Object & Requesting Garbage Collection=====");
+	for (int i = 0; i<s.length; i++)
+		s[i] = null;
+	System.gc();
+	Thread.sleep(500);
+		
+	}
+}
+	
+
+
+
+
+
+
+
